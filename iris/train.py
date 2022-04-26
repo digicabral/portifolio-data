e6227610930507c6f4f931868e29b4b50cc49307
Rodrigo Cabral <digicabral@gmail.com>
2022-04-25 21:37:39 -0300

    Modularizado

--- iris/train.py
+++ iris/train.py
@@ -5,13 +5,31 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 
-data = load_iris()
+#Extract data
+def extract_data():
+    data = load_iris()
+    return data
 
-X = pd.DataFrame(data.data, columns=data.feature_names)
-y = pd.DataFrame(data.target, columns=["target"])
+#Preparing features
+def preparing_features(data):
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = pd.DataFrame(data.target, columns=["target"])
+    return X, y
 
-model = DecisionTreeClassifier(max_depth=2)
-model.fit(X,y)
+#Training model
+def train_model(X,y):
+    model = DecisionTreeClassifier(max_depth=2)
+    model.fit(X,y)
+    return model
 
-with open("trained_classifier.pkl","wb") as file:
-    pickle.dump(model, file)
+# Serialize object
+def serialize_object(model):
+    with open("trained_classifier.pkl","wb") as file:
+        pickle.dump(model, file)
+
+data = extract_data()
+X, y = preparing_features(data)
+model = train_model(X,y)
+serialize_object(model=model)
+
+
